de3453c3f2a3ef344efe75c2a5223a4cf851908b
bnglm1 <137788571+bnglm1@users.noreply.github.com>
2023-10-08 17:32:41 +0300

    Add files via upload

---
+++ First/d1.js
@@ -0,0 +1,13 @@
+//write a java code for writing helloworld?
+document.body.appendChild(document.createTextNode('Hello World!'));
+
+
+
+//Source: https://stackoverflow.com/questions/41482348
+
+
+
+
+
+
+
